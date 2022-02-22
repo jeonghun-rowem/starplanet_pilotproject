c20e5e59178e671bdc1a079d38ed59ed559e8d2c
jeonghun-rowem <lyju777@rowem.com>
2022-02-22 17:25:52 +0900

    스타패스 해더 및 메인화면 레이아웃 작업

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <HeaderBar />
+    <MaIn />
+  </v-app>
+</template>
+
+
+<script>
+
+import HeaderBar from './components/HeaderBar';
+import MaIn from './components/main';
+
+export default {
+  name: 'App',
+
+  components: {
+    HeaderBar,MaIn,
+ 
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/main.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="first_m">
+      <v-img
+        class="first_m_starpass_logo"
+        src="../assets/1.starpass_logo.png"
+      />
+
+      <v-img
+        class="first_m_star_plannet"
+        src="../assets/2.star_planet.png"
+      />
+      <div class="first_m_text1">
+        <span class="first_m_text1_1">
+          K-Pop, 트로트 팬덤을 위한 
+        </span>
+        <span class="first_m_text1_2">
+          글로벌 플랫폼
+        </span>
+        <span class="first_m_text1_3">
+          !
+        </span>
+      </div>
+      <div class="first_m_text2">
+        <p>안드로이드,iOS 모두 지원</p>
+      </div>
+    </div>
+    <div class="second_m" />
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'MaIn'
+}
+</script>
+
+<style>
+@import '../static/Main.css';
+</style>
