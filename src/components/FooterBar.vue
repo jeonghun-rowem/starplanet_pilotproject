764d077dad7682c9d308e601941dd0e347c818cf
jeonghun-rowem <lyju777@rowem.com>
2022-02-23 16:54:33 +0900

    footer페이지 레이아웃 및 css작업

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,7 +1,39 @@
 <template>
   <footer>
     <div class="footer">
-      <div />
+      <v-img
+        class="footer_img1"
+        src="../assets/3.star_planet_logo.png"
+      />
+      <div class="footer_text1">
+        <span class="footer_text1_1">회사소개</span>
+        <span class="footer_text1_2">서비스 이용약관</span>
+        <span class="footer_text1_3">개인정보 처리방침</span>
+      </div>
+      <div class="footer_text2">
+        <span class="footer_text2_1">(주)로웸</span>
+        <span class="footer_text2_2">서울특별시 구로구 디지털로 32길 30 코오롱디지털타워빌란트 1015호 (08390)&emsp;|&emsp;사업자등록번호 : </span>
+        <span class="footer_text2_3">113-86-58272</span>
+        <span class="footer_text2_4">
+          &emsp;|&emsp;통신판매업신고 : 구로 1762호
+          &emsp;|&emsp;대표자명 : 권오승</span>
+      </div>
+      <div class="footer_text3">
+        <span class="footer_text3_1">
+          고객문의:
+        </span>
+        <span class="footer_text3_2">
+          starplanet@rowem.com
+        </span>
+        <span class="footer_text3_3">
+          &emsp;|&emsp;고객센터 : 02-2103-5131
+        </span>
+      </div>
+      <div class="footer_text4">
+        <span class="footer_text4_1">
+          © ROWEM Corp. All Rights Reserved.
+        </span>
+      </div>
     </div>
   </footer>
 </template>
