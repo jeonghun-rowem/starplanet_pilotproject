743138cc58ebc6a7683dd55a1c944c988f962297
jeonghun-rowem <lyju777@rowem.com>
2022-02-28 12:06:22 +0900

    메인페이지 scroll up 기능구현

--- src/components/ScrollUp.vue
+++ src/components/ScrollUp.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="scrollup" />
+  <div
+    class="scrollup"
+    @click="scrollup()"
+  />
 </template>
 
 <script>
 export default {
-    name : 'ScrollUp'
+    name : 'ScrollUp',
+
+    methods :{
+      // 스크롤 위치 최상단으로 보내기 behavior: "smooth"를 사용한 부드러운 이동
+      scrollup(){
+        window.scrollTo({top:0, left:0, behavior: "smooth"})  
+      },
+
+    }
 }
 </script>
 
