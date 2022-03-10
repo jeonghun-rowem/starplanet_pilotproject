fb48e7dff0456dbd8a2d94935bf9a2673687b7c2
lyju777 <lyju777@hanmail.net>
2022-03-10 22:26:04 +0900

    index.scss파일 생성 main.js등록

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import App from "@/App.vue";
 import vuetify from "@/plugins/vuetify.js";
 import router from "@/router/index.js";
+// 전역scss 설정
+import index from "@/static/index.scss";
 
 Vue.config.productionTip = false;
 
@@ -10,5 +12,6 @@ new Vue({
   vuetify,
   router,
   axios,
+  index,
   render: (h) => h(App),
 }).$mount("#app");
